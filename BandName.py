0bcb8f336aaf4df85493fc7c5c0b85c477ecf3f4
rachit2406 <134596004+rachit2406@users.noreply.github.com>
2024-09-09 00:58:50 +0530

    Add files via upload
    For your first Python project, you created a band name generator, a fun and creative program that combines user inputs to suggest unique band names. The program begins by asking the user for two pieces of information: the name of the city they admire and their name. By merging these inputs, the program generates a quirky and personalized band name, sparking creativity and imagination.
    
    You designed the project to be simple yet engaging, using basic concepts like variables, user input, and string concatenation. The band name generator not only serves as an introduction to coding but also showcases how even the most basic tools in Python can be used to create something entertaining. It encourages users to experiment by inputting different combinations, making it interactive and enjoyable.
    
    Overall, the project highlights your early steps in programming, where you blend logic with creativity. It’s a great starting point for learning more about functions, handling user input, and enhancing your coding skills in future projects.

---
+++ BandName.py
@@ -0,0 +1,4 @@
+print("********** WELCOME TO BAND NAME GENERATOR **********")
+U1 = input("Enter your name : ")
+U2 = input("Enter the place which you admire : ")
+print("Your band name could be " + U1 + " " + U2)
